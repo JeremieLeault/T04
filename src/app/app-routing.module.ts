9b09fa1de32709854b015d3c600af9dcb7648639
JeremieLeault <49938455+JeremieLeault@users.noreply.github.com>
2022-01-06 10:44:26 +0100

    premier commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './webApp/root/accueil/home/home.component';
+import { ListeDesFilmsComponent } from './webApp/root/films/liste-des-films/liste-des-films.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component:HomeComponent},
+  { path:'liste-des-films', component:ListeDesFilmsComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccueilModule } from './webApp/root/accueil/accueil.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // import de nos Modules
+    AccueilModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
